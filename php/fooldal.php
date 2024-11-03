59b8eae3ed5ceabc47c92ec73f0827d2a90c4224
ivianna <ivanyianna05@gmail.com>
2024-11-03 16:47:16 +0100

    Hibajavítások

---
+++ php/fooldal.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="hu">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Kezdőlap</title>
+    <link rel="shortcut icon" href="../img/eKurzusok.png" type="image/x-icon">
+    <link rel="stylesheet" href="../css/fooldal.css">
+</head>
+<body>
+    <div class="header">
+        <div class="header_buttons">
+            <!-- <input type="button" id="login_button" value="Bejelentkezés" onclick="openLogin()"/> -->
+            <a href="../php/login.php" target="_self" id="login_button">Bejelentkezés</a>
+            <!-- <input type="button" id="signup_button" value="Regisztráció" onclick="openSignup()"/> -->
+            <a href="../php/signup.php" target="_self" id="signup_button">Regisztráció</a>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="links">
+            <a href="#">Rólunk</a>
+            <a href="#">Kapcsolat</a>
+            <a href="#">Általános Felhasználási Feltételek</a>
+            <a href="#">Adatnyilatkozat</a>
+        </div>
+    </div>
+    <script src="../js/function.js"></script>
+</body>
+</html>
