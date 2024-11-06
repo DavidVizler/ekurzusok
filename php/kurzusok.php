4f570b607fe7b1b1d2b53cc1b7f16bbf7121c300
ivianna <ivanyianna05@gmail.com>
2024-11-06 20:23:13 +0100

    PopUp szerkesztése

--- php/kurzusok.php
+++ php/kurzusok.php
@@ -52,14 +52,20 @@
     <!-- Popup -->
     <div id="popup" class="popup-overlay" style="display:none">
         <div class="popup-content">
-            <span class="close-btn" onclick="closePopup()">&times;</span>
+            <a class="close-btn" onclick="closePopup()">
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+                    <path stroke-linecap="round" stroke-linejoin="round" d="M6 18 18 6M6 6l12 12" />
+                </svg>
+            </a>
             <div class="popup-content-title">
-                <h2>Kurzus felvétele</h2>
-            </div><hr id="hr">
+                <h3>Kurzus felvétele</h3>
+            </div>
+            <hr>
             <div class="courseCodeAdd">
                 <h7>Kurzus kód megadása: </h7>
                 <input type="text" name="coursedCode">
-            </div><hr id="hr">
+            </div>
+            <hr>
             <div class="createCourse">
                 <a href="">Kurzus létrehozása</a>
             </div>
