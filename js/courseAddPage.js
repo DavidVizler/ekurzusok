21d1f0c6fc8fe18209abd91c70956b0760fc6bde
ivianna <ivanyianna05@gmail.com>
2024-11-18 19:54:46 +0100

    Kurzus megjelenítése a kurzusok oldalon dinamikus adatgenerálással

--- js/courseAddPage.js
+++ js/courseAddPage.js
@@ -18,15 +18,45 @@ function previewLoad(){
     let selectDesign = document.querySelector("#DesignSelect")
     let designName = selectDesign.options[selectDesign.selectedIndex].text;
     let preview = document.getElementById("preView")
-    let previewImage = document.getElementById("preViewImage")
-    previewImage.innerHTML = ""
+    let kurzusNev = document.getElementById("KurzusNev")
+    let oktatoNeve = document.getElementById("OktatoNeve")
+    preview.innerHTML = ""
     design.forEach(elem => {
         if(elem.designName == designName){
             preview.style.display = "flex"
-            previewImage.innerHTML += `<img src="${elem.image}" alt="Kurzus téma képe">`
+            preview.style.justifyContent = "center"
+            preview.style.alignItems = "center"
+            let card = document.createElement("div")
+            card.classList.add("card")
+
+            let cardHeader = document.createElement("div")
+            cardHeader.classList.add("card-header")
+
+            let img = document.createElement("img")
+            img.src = elem.image
+            img.alt = "Kurzus témája"
+            img.classList.add("card-img-top")
+
+            let cardBody = document.createElement("div")
+            cardBody.classList.add("card-body")
+
+            let cardTitle = document.createElement("h5")
+            cardTitle.textContent = kurzusNev.value
+            cardTitle.classList.add("card-title")
+
+            let cardOktatok = document.createElement("h6")
+            cardOktatok.classList.add("card-teachers")
+            cardOktatok.textContent = oktatoNeve.value
+
+            cardBody.appendChild(cardTitle)
+            cardBody.appendChild(cardOktatok)
+            cardHeader.appendChild(img)
+            card.appendChild(cardHeader)
+            card.appendChild(cardBody)
+            preview.appendChild(card)
         }
     });
 }
 
-document.getElementById("DesignSelect").addEventListener('click', previewLoad)
+document.getElementById("DesignSelect").addEventListener('change', previewLoad)
 window.addEventListener('load', designOptionLoad)
--- php/courseAdd_manager.php
+++ php/courseAdd_manager.php
@@ -2,21 +2,39 @@
     include './sql_fuggvenyek.php';
 
     function createCourse(){
-        if($_SERVER["REQUEST_METHOD"] == "POST"){
-            if(isset($_POST["createButton"])){
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+            if (isset($_POST["createButton"])) {
                 $kurzusNev = $_POST["KurzusNev"];
                 $oktatok = $_POST["OktatoNeve"];
                 $leiras = $_POST["Leiras"];
                 $design = $_POST["DesignSelect"];
-                $kurzusKod = GenerateCourseCode();
-                if(!empty($kurzusNev) && !empty($oktatok) && !empty($leiras) && !empty($design)){
+
+                if (!empty($kurzusNev) && !empty($oktatok) && !empty($leiras) && !empty($design)) {
+                    $kurzusKod = GenerateCourseCode();
                     $courseAdd_sql = "INSERT INTO kurzus(FelhasznaloID, KurzusNev, Oktatok, Kod, Leiras, Design) VALUES(13,'{$kurzusNev}','{$oktatok}', '{$kurzusKod}', '{$leiras}', {$design})";
                     $courseAdd = AdatModositas($courseAdd_sql);
-                    echo $courseAdd;
                 }
             }
         }
+
     }
+
+    function selectCardData(){
+        if($_SERVER["REQUEST_METHOD"] == "GET"){
+            $cardDatas_sql = "SELECT KurzusID, KurzusNev, Oktatok, Kod, Leiras, Design FROM kurzus";
+            $cardDatas = AdatLekerdezes($cardDatas_sql);
+            if(is_array($cardDatas)){
+                echo json_encode($cardDatas,JSON_UNESCAPED_UNICODE);
+            }else{
+                header("BAD REQUEST", true, 400);
+                echo json_encode(["valasz" => "Nincsenek találatok!"],JSON_UNESCAPED_UNICODE);
+            }
+        }else{
+            header("BAD REQUEST",true, 400);
+            echo json_encode(["valasz" => "Hibás metódus!"],JSON_UNESCAPED_UNICODE);
+        }
+    }
+    selectCardData();
 
     function GenerateCourseCode(){
         $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
