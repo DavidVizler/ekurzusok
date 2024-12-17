b64bc524eb2f00b78265bcbd9762ba02251ddecd
ivianna <ivanyianna05@gmail.com>
2024-12-17 10:31:02 +0100

    Settings változtatások

--- js/settings.js
+++ js/settings.js
@@ -2,3 +2,36 @@ let pageLink = document.getElementById("backtoPage")
 pageLink.addEventListener("click", ()=>{
     history.back()
 })
+
+
+async function getDesign() {
+    let response = await fetch("../js/desgin.json")
+    let design = await response.json()
+    return design
+}
+
+async function loadDesign() {
+    let designData = await getDesign()
+    let designSelect = document.getElementById("designSelect")
+    for (let data of designData) {
+        let option = document.createElement("option")
+        option.value = data.designId
+        option.textContent = data.designName
+        designSelect.appendChild(option)
+    }
+}
+
+async function loadPreview() {
+    let designData = await getDesign()
+    let selectedDesign = document.getElementById("designSelect").value
+    let previewDiv = document.getElementById("previewDiv")
+
+    for(let data of designData){
+        if(data.designId == selectedDesign){
+            previewDiv.style.backgroundImage = `url('${data.image}')`
+        }
+    }
+}
+
+window.addEventListener('load', loadDesign)
+document.getElementById("designSelect").addEventListener("change", loadPreview)
