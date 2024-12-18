cea72c9422e213bb394e2f723d30a9885803b744
ivianna <ivanyianna05@gmail.com>
2024-12-18 17:39:57 +0100

    Kurzus beállításai felület

--- js/settings.js
+++ js/settings.js
@@ -25,13 +25,28 @@ async function loadPreview() {
     let designData = await getDesign()
     let selectedDesign = document.getElementById("designSelect").value
     let previewDiv = document.getElementById("previewDiv")
-    
+
     for(let data of designData){
         if(data.designId == selectedDesign){
+            previewDiv.style.display = "flex"
             previewDiv.style.backgroundImage = `url(${data.courseImage})`
         }
     }
 }
 
+async function toggleArrow(){
+    let arrow = document.querySelector('.arrow')
+    let previewDiv = document.getElementById("previewDiv")
+
+    arrow.classList.toggle('flipped')
+
+    if (previewDiv.style.display === "none" || previewDiv.style.display === "") {
+        previewDiv.style.display = "flex";
+    } else {
+        previewDiv.style.display = "none";
+    }
+}
+
 window.addEventListener('load', loadDesign)
 document.getElementById("designSelect").addEventListener("change", loadPreview)
+document.querySelector(".openPreviewDiv").addEventListener('click', toggleArrow)
