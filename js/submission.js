5a560375420a5a23f1c6a4de90d53d7de83b818f
ivianna <ivanyianna05@gmail.com>
2025-03-30 19:22:43 +0200

    Beadott munkák megjelenítése

--- js/submission.js
+++ js/submission.js
@@ -36,12 +36,12 @@ function showSubedData(adatok) {
         div.style.alignItems = 'center';
         div.style.gap = "15px";
 
-        let h1 = document.createElement("h1");
+        let h1 = document.createElement("h2");
         h1.innerHTML = adat.lastname + " " + adat.firstname;
 
         let div2 = document.createElement("div");
         div2.classList.add("open");
-        div2.addEventListener('click', toggleArrow);
+        div2.addEventListener('click', (event) => toggleArrow(event, adat.submission_id)); 
 
         let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
         svg.classList.add("arrow");
@@ -63,10 +63,19 @@ function showSubedData(adatok) {
         path2.setAttribute("d", "m4.5 12.75 7.5-7.5 7.5 7.5");
 
         let datas = document.createElement("div")
-        datas.id = "datasDiv"
+        datas.id = `datasDiv-${adat.submission_id}`;
+        datas.style.display =  "none"
+        datas.classList.add("datasDiv")
 
         datas.innerHTML = "Beadva: " + adat.submitted + "<br>"
+        datas.innerHTML += "Beadott fájlok száma: " + adat.files_count + "<br>"
         datas.innerHTML += "Fájlok: "
+
+        let hiddenSubId = document.createElement("input")
+        hiddenSubId.type = "number"
+        hiddenSubId.value = adat.submission_id
+        hiddenSubId.id = "subId"
+        hiddenSubId.hidden = true
 
         div.appendChild(h1);
         div.appendChild(div2);
@@ -76,14 +85,85 @@ function showSubedData(adatok) {
 
         dataDiv.appendChild(div);
         dataDiv.appendChild(datas)
+        dataDiv.appendChild(hiddenSubId)
+
+        let hr = document.createElement("hr");
+        dataDiv.appendChild(hr);
+
         ki.appendChild(dataDiv)
     }
 }
 
-function toggleArrow(event) {
+function toggleArrow(event, submissionId) {
     let arrow = event.currentTarget.querySelector('.arrow');
+    let previewDiv = document.getElementById(`datasDiv-${submissionId}`);
+
     if (arrow) {
         arrow.classList.toggle('flipped');
+    }
+
+    if (previewDiv.style.display === "none" || previewDiv.style.display === "") {
+        previewDiv.style.display = "flex";
+        setTimeout(() => previewDiv.classList.add("active"),5);
+        GetSubmittedFiles(submissionId);
+    } else {
+        previewDiv.classList.remove("active");
+        setTimeout(() => (previewDiv.style.display = "none"), 300);
+    }
+}
+
+async function GetSubmittedFiles(submissionId) {
+    console.log("Lekérendő Submission ID:", submissionId);
+
+    try {
+        let request = await fetch("api/query/submission-files", {
+            method: "POST",
+            headers: { "Content-Type": "application/json" },
+            body: JSON.stringify({ "submission_id": submissionId })
+        });
+
+        let response = await request.json();
+        console.log(response);
+        showFiles(response, submissionId);
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+function showFiles(files, submissionId) {
+    let ki = document.querySelector(`#datasDiv-${submissionId}`);
+    
+    if (ki.querySelectorAll(".fileDiv").length > 0) {
+        console.log("Fájlok már megvannak jelenítve.");
+        return;
+    }
+
+    for (let file of files) {
+        let fileDiv = document.createElement("div");
+        fileDiv.classList.add("fileDiv");
+        fileDiv.id = "fileDiv" + file.file_id;
+
+        let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
+        svg.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+        svg.setAttribute("fill", "none");
+        svg.setAttribute("viewBox", "0 0 24 24");
+        svg.setAttribute("stroke-width", "1.5");
+        svg.setAttribute("stroke", "currentColor");
+        svg.classList.add("size-6");
+
+        let path = document.createElementNS('http://www.w3.org/2000/svg', 'path');
+        path.setAttribute("stroke-linecap", "round");
+        path.setAttribute("stroke-linejoin", "round");
+        path.setAttribute("d", "M3.75 6.75h16.5M3.75 12H12m-8.25 5.25h16.5");
+
+        let h1 = document.createElement("h1");
+        h1.classList.add("fileName");
+        h1.innerHTML = file.name;
+
+        fileDiv.appendChild(svg);
+        svg.appendChild(path);
+        fileDiv.appendChild(h1);
+        ki.appendChild(fileDiv);
     }
 }
 
